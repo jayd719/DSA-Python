1fae31a4b10bacb2617af6b5f2b07618cfeb1c85
JD-MBP <jashansingh29@gmail.com>
2024-05-09 21:21:32 -0400

    day3

--- subarraySum.py
+++ subarraySum.py
@@ -20,7 +20,16 @@ def sumOfSubArray(arr,sum):
 def slidingWindow(arr,sum):
     currSum = arr[0]
     start =0
-    i=0
+    i =1
+    while(i<len(arr)):
+        if(currSum==sum):
+            return arr[start:i]
+        currSum+=arr[i]
+        while(currSum>sum and start<i-1):
+            currSum-=arr[start]
+            start+=1    
+        
+        i+=1
 
 
 
@@ -28,5 +37,9 @@ def slidingWindow(arr,sum):
 test1 =[1, 4, 0, 0, 3, 10, 5]
 test2 =[1, 4, 20, 3, 10, 5]
 
-print(sumOfSubArray(test1,5))
+print(sumOfSubArray(test1,7))
 print(sumOfSubArray(test2,13))
+
+print('\n\n')
+print(slidingWindow([1, 2, 3, 4, 5],9))
+print(slidingWindow(test2,13))
